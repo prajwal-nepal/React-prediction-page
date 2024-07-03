bb94ffae5351bbbfee5664ba0b1ebdce6b752b84
Prajwal <nprajwal21@tbc.edu.np>
2024-07-03 09:35:04 +0545

    Porject commit

---
+++ app/prediction/[name]/page.tsx
@@ -0,0 +1,58 @@
+// Function to fetch the predicted age based on the given name
+
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io?name=${name}`);
+  return res.json();
+};
+
+// Function to fetch the predicted gender based on the given name
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
+
+// Function to fetch the predicted nationality based on the given name
+
+const getPredictedNationality = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
+interface Params {
+  params: { name: string };
+}
+
+// Main prediction component
+async function Prediction({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const nationalityData = getPredictedNationality(params.name);
+  const [age, gender, nationality] = await Promise.all([
+    ageData,
+    genderData,
+    nationalityData,
+  ]);
+
+    // Return the JSX for displaying the predictions
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          Personal Info
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Age: {age?.age}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Gender: {gender?.gender}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Nationality: {nationality?.country[0]?.country_id}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Prediction;
